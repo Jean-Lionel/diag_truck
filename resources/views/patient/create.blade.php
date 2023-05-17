9625034467f63c7269df9745a2ea3b16b59c17fe
Tuno27 <cizanoella27@gmail.com>
2023-05-17 18:51:02 +0200

    Adding dashboard

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 @if ($message = Session::get('success'))
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.dashboard');
 });
 
 Auth::routes();
