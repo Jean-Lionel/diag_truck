71f8297fe6fe9c548742b967aef00f0265c83b55
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-20 10:16:20 +0200

    some changes

--- database/migrations/2023_05_15_061045_create_users_table.php
+++ database/migrations/2023_05_15_061045_create_users_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('lname')->nullable();
+            $table->string('lastName')->nullable();
             $table->string('phone')->nullable();
             $table->string('sexe')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('groupe')->nullable();
+            $table->enum('role_name', ['INFIRMIER', 'ADMIN', 'DOCTEUR'])->nullable();
             $table->string('specialite_Docteur')->nullable();
             $table->string('qualification_infirmier')->nullable();
             $table->boolean('statut')->nullable();
