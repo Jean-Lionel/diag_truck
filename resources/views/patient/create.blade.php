bb50cee72fe92259ddd7bd50685c7301bebbd1cd
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-20 09:50:51 +0200

    base initial commit

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.dashboard')
 
 @section('content')
-@if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
-        @if ($message = Session::get('error'))
-            <div class="alert alert-danger">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    @if ($message = Session::get('error'))
+        <div class="alert alert-danger">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
     <form action="{{ route('patient.store') }}" method="post">
         @method('POST')
         @include('patient.form', ['title' => 'Ajouter un Patient'])
