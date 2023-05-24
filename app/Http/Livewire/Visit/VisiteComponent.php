241d59fe7cd32ea61fc01d345813d9e263008d74
Jean-Lionel <nijeanlionel@gmail.com>
2023-05-24 11:21:08 +0200

    add of dashboard

---
+++ app/Http/Livewire/Visit/VisiteComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Visit;
+
+use Livewire\Component;
+
+class VisiteComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.visit.visite-component');
+    }
+}
