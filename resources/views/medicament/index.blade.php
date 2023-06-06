9ad79e2a934ef0f96ec96eb6f676d009d405d618
Jean-Lionel <nijeanlionel@gmail.com>
2023-06-06 08:55:06 +0200

    medicament

--- resources/views/medicament/index.blade.php
+++ resources/views/medicament/index.blade.php
@@ -2,61 +2,59 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card" style="padding: 0;">
-        <div class="card-header">
+<div class="card" style="padding: 0;">
+    <div class="card-header">
 
-            <div class="d-flex justify-content-between">
-                <h5 class="card-title">Liste des Medicaments</h5>
-                <a href="{{ route('medicament.create') }}" type="button" class="btn btn-primary btn-block">
-                    <i class="bi bi-plus-circle"></i> Ajouter
-                </a>
-            </div>
-        </div>
-        <div class="card-body">
-            <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <th class="th-sm">#
-                        </th>
-
-                        <th class="th-sm">Nom
-                        </th>
-
-                        <th class="th-sm">Déscription
-                        </th>
-
-                        <th class="th-sm">Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    @foreach ($medicaments as $item)
-                        <tr>
-                            <td> {{ ++$loop->index }}</td>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->description }}</td>
-
-                            <td>
-                                <a href="{{ route('medicament.edit', $item) }}" class="btn-sm btn btn-primary"
-                                    title="Modifier">
-                                    <i class="bi bi-pencil-square"></i>
-                                </a>
-
-                                <a href="{{ route('medicament.destroy', $item) }}" title="Supprimer"
-                                    class="btn-sm btn btn-danger">
-                                    <i class="bi bi-trash" title="Supprimer"></i>
-                                </a>
-
-
-                            </td>
-                        </tr>
-                    @endforeach
-
-
-                </tbody>
-
-            </table>
+        <div class="d-flex justify-content-between">
+            <h5 class="card-title">Liste des Medicaments</h5>
+            <a href="{{ route('medicament.create') }}" type="button" class="btn btn-primary btn-block">
+                <i class="bi bi-plus-circle"></i> Ajouter
+            </a>
         </div>
     </div>
+    <div class="card-body">
+        <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th class="th-sm">#
+                    </th>
+
+                    <th class="th-sm">Nom
+                    </th>
+
+                    <th class="th-sm">Déscription
+                    </th>
+
+                    <th class="th-sm">Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($medicaments as $item)
+                <tr>
+                    <td> {{ ++$loop->index }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+
+                    <td>
+                        <a href="{{ route('medicament.edit', $item) }}" class="btn-sm btn btn-primary"
+                        title="Modifier">
+                        <i class="bi bi-pencil-square"></i>
+                    </a>
+                    <form class="form-delete" action="{{ route('medicament.destroy' , $item) }}" style="display: inline;" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button class="btn btn-outline-danger btn-sm delete_client">Supprimer</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+
+
+        </tbody>
+
+    </table>
+</div>
+</div>
 @endsection
