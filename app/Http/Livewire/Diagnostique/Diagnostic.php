de9383f73fc5dec96c3dbd7267af743f6d67f044
Jean-Lionel <nijeanlionel@gmail.com>
2023-06-06 10:14:11 +0200

    diagnostic

--- app/Http/Livewire/Diagnostique/Diagnostic.php
+++ app/Http/Livewire/Diagnostique/Diagnostic.php
@@ -8,6 +8,9 @@ use App\Models\Patient;
 class Diagnostic extends Component
 {
     public $numeroPatient;
+    public $patient;
+    public $diagnostique;
+    public $contenu;
     public function render()
     {
         return view('livewire.diagnostique.diagnostic');
@@ -15,8 +18,16 @@ class Diagnostic extends Component
 
     public function getPatient()
     {
-        $patient = Patient::where('id', $this->numeroPatient)->first()->get();
+        $this->patient = Patient::where('id', $this->numeroPatient)->first()->get();
+    }
 
-        dd($patient);
+    public function saveDiagnostique()
+    {
+        Diagnostic::create([
+            'patient_id' => $this->patient->id,
+            'docteur_id' => auth()->user()->id,
+            'contenu' => $this->contenu,
+            'date_diag' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
