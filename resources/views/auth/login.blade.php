27541ae04a8532ffb4b44fa97b4242035b6ef114
Jean-Lionel <nijeanlionel@gmail.com>
2023-06-12 08:44:09 +0200

    add dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,131 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <style>
-        .container{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-    </style>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <style>
+            .container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                width: 800px;
+            }
+
+            .card {
+                width: 100%;
+                padding: 30px;
+            }
+
+            body {
+                background-image: linear-gradient(135deg, #FAB2FF 10%, #1904E5 100%);
+            }
+
+            .card_header {
+                text-align: center;
+                font-size: 35px;
+                font-weight: 800;
+            }
+
+            input {
+                width: 300px;
+                height: 45px;
+            }
+
+            button {
+                width: 100%;
+                height: 45px;
+                font-size: 25px;
+            }
+
+            label {
+                margin-bottom: 12px;
+
+     font-size: 15px;
+                font-weight: 700;
+            }
+        </style>
+    </head>
+
+    <body>
 
 
-<div class="container">
-    <div class="row ">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <div class="container">
+            <div class="row ">
+                <div class="col">
+                    <div class="card">
+                        <div class="card_header">{{ __('Connexion') }}</div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                <div class="row mb-3">
+                                    <label for="email" class="">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <div class="col-md-12">
+                                        <input id="email" type="email"
+                                            class="form-control @error('email') is-invalid @enderror" name="email"
+                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                <div class="row mb-3">
+                                    <label for="password" class="">{{ __('Password') }}</label>
 
-                            </div>
+                                    <div class="col-md-12">
+                                        <input id="password" type="password"
+                                            class="form-control @error('password') is-invalid @enderror" name="password"
+                                            required autocomplete="current-password">
+
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember"
+                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="">
+                                    <div class="">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Se connecter') }}
+                                        </button>
+
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
 
 
-</body>
+    </body>
+
 </html>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,1 +1,150 @@
-@extends("layouts.app")
+{{-- @extends('layouts.app') --}}
+<style>
+    .dashboard_container{
+        margin:0px 20px;
+    }
+    .top_cards{
+        display:flex;
+        width:100%;
+        justify-content:space-between;
+        flex-wrap:wrap;
+    }
+    .card{
+        width:250px;
+        height:600px;
+        padding:40px 5px;
+    }
+    .card h3{
+        font-size:20px;
+        text-align:center;
+    }
+    .bar{
+        height:20px;
+        border-radius:3px;
+        width:100%;
+        margin-bottom:15px;
+        background-color:white;
+    }
+    table{
+        background-color:white;
+
+    }
+</style>
+@extends('layouts.app')
+
+@section('content')
+   <div class="dashboard_container">
+       <div class="top_cards">
+            <div class="card">
+                <h3>Patients</h3>
+                <h3>200</h3>
+            </div>
+            <div class="card">
+                <h3>Dossiers</h3>
+                <h3>200</h3>
+            </div>
+            <div class="card">
+                <h3>Assignations</h3>
+                <h3>200</h3>
+            </div>
+            <div class="card">
+                <h3>Prescriptions</h3>
+                <h3>200</h3>
+            </div>
+       </div>
+       <div class="bar">
+            <p></p>
+       </div>
+       <h4>Personnels</h4>
+       <div class="card-body">
+            <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th class="th-sm">#
+                        </th>
+                        <th class="th-sm">Nom
+                        </th>
+                        <th class="th-sm">Prénom
+                        </th>
+                        <th class="th-sm">Telephone
+                        </th>
+                        <th class="th-sm">Email
+                        </th>
+                        <th class="th-sm">Role
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                           <td>1</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                        </tr>
+                        <tr>
+                           <td>1</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                        </tr>
+                        <tr>
+                           <td>1</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                        </tr>
+                        <tr>
+                           <td>1</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                        </tr>
+
+
+
+                </tbody>
+
+            </table>
+        </div>
+        <div class="bar">
+            <p></p>
+       </div>
+        <h4>Services</h4>
+       <div class="card-body">
+            <table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th class="th-sm">#
+                        </th>
+                        <th class="th-sm">Nom
+                        </th>
+                        <th class="th-sm">Statut
+                        </th>
+                        <th class="th-sm">Description
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                           <td>1</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                           <td>asdasdsad</td>
+                        </tr>
+
+
+                </tbody>
+
+            </table>
+        </div>
+   </div>
+@endsection
